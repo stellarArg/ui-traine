e1cfc0335fa6d981d1bb15fb44ceb4d359cad53c
Gabriel Verges <stellarusiv@gmail.com>
2020-03-20 13:39:56 -0300

    feat(fontawesome): add fontawesome

--- src/components/table.js
+++ src/components/table.js
@@ -2,29 +2,34 @@ import React from 'react';
 import map from 'lodash/map';
 import get from 'lodash/get';
 import {Link} from 'react-router-dom';
+import Button from 'reactstrap/lib/Button';
 import Table from 'reactstrap/lib/Table';
 
 export default ({data, columns, headers, onSort}) => (
-    <Table striped size="sm" hovered>
+    <Table bordered size="sm" hovered dark>
+        <thead>
         <tr>
             {map(headers, header => (
                 <th onClick={() => onSort(header)}>
                     {header.label}
-                    {header.sort === 'desc' &&  (<i className="glyphicon glyphicon-menu-down float-right"/>)}
-                    {header.sort !== 'desc' && (<i className="glyphicon glyphicon-menu-up float-right"/>)}
+                    {header.sort === 'desc' &&  (<i className="fas fa-angle-up float-right"/>)}
+                    {header.sort !== 'desc' && (<i className="fas fa-angle-down float-right"/>)}
                 </th>
             ))}
         </tr>
-        {map(data, d => (
-            <tr key={d.id}>
-                {map(columns, column => {
-                    if(column === 'actions') {
-                        return (<td><Link to={`/country/edit/${d.code}`}> Edicion </Link></td>);
-                    }
-                    
-                    return (<td>{get(d, column)}</td>);
-                })}
-            </tr>
-        ))}
+        </thead>
+        <tbody>
+            {map(data, d => (
+                <tr key={d.id}>
+                    {map(columns, column => {
+                        if(column === 'actions') {
+                            return (<td><Button tag={Link} color="primary" to={`/country/edit/${d.code}`}> Edicion </Button></td>);
+                        }
+                        
+                        return (<td>{get(d, column)}</td>);
+                    })}
+                </tr>
+            ))}
+        </tbody>
     </Table>
 )
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 import Root from './containers/Root';
 import configureStore from './store';
